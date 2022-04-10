7e60869a9d2e6d8d3ea5ba89704a254f6cf814aa
Dita Permata Putra <ditateum@gmail.com>
2022-04-10 12:34:50 +0700

    create data model and generate with build_runner

---
+++ lib/model/data_model.dart
@@ -0,0 +1,28 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'data_model.freezed.dart';
+part 'data_model.g.dart';
+
+@freezed
+class DataModel with _$DataModel {
+  factory DataModel({
+    required int id,
+    required String title,
+    required String worth,
+    required String thumbnail,
+    required String image,
+    required String description,
+    required String instructions,
+    required String openGiveawayUrl,
+    required String publishedDate,
+    required String type,
+    required String platforms,
+    required String endDate,
+    required int users,
+    required String status,
+    required String gamePowerUrl,
+  }) = _DataModel;
+
+  factory DataModel.fromJson(Map<String, dynamic> json) =>
+      _$DataModelFromJson(json);
+}
